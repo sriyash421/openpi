cd6b1a59a8b3f476f5d9813337794fbe2ea983c2
Jesse Zhang <jessezhang5@gmail.com>
2025-04-04 11:03:48 -0700

    using path utils for labelign data and also imported it into the libero_policy

--- examples/libero/convert_pathmask_libero_data_to_lerobot.py
+++ examples/libero/convert_pathmask_libero_data_to_lerobot.py
@@ -20,6 +20,7 @@ Running this conversion script will take approximately 30 minutes.
 import shutil
 from pathlib import Path
 
+from openpi.policies.mask_path_utils import get_mask_and_path_from_h5
 from lerobot.common.datasets.lerobot_dataset import LEROBOT_HOME
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 import h5py
@@ -28,7 +29,6 @@ import tyro
 import os
 import json
 
-# TODO: iterate through the dataset_movement_and_masks.h5 file and extract the masks and paths.
 RAW_DATASET_NAMES = [
     "libero_90",
     # "libero_10",
@@ -40,7 +40,14 @@ assert len(RAW_DATASET_NAMES) == 1, "Only one dataset name is supported at a tim
 REPO_NAME = "jesbu1/libero_90_lerobot_pathmask"  # Name of the output dataset, also used for the Hugging Face Hub
 
 
-def main(data_dir: str, path_and_mask_file_dir: str, *, push_to_hub: bool = False):
+def main(
+    data_dir: str,
+    path_and_mask_file_dir: str,
+    *,
+    push_to_hub: bool = False,
+    use_subtask_instructions: bool = False,
+    use_subtask_path: bool = True,
+):
     # Clean up any existing dataset in the output directory
     output_path = LEROBOT_HOME / REPO_NAME
     if output_path.exists():
@@ -93,47 +100,86 @@ def main(data_dir: str, path_and_mask_file_dir: str, *, push_to_hub: bool = Fals
 
     # Loop over raw Libero datasets and write episodes to the LeRobot dataset
     # You can modify this for your own data format
-    with h5py.File(Path(path_and_mask_file_dir) / "dataset_movement_and_masks.h5", "r") as points_and_masks_h5:
-        for raw_dataset_name in RAW_DATASET_NAMES:
-            # open the directory containing the h5 files using raw_dataset_name
-            libero_h5_list = [file for file in os.listdir(Path(data_dir) / raw_dataset_name) if file.endswith(".hdf5")]
-            for libero_h5_file in libero_h5_list:
-                with h5py.File(Path(data_dir) / raw_dataset_name / libero_h5_file, "r") as f:
-                    for demo_num in enumerate(f["data"].keys()):
-                        for observation in f["data"][demo_num]["obs"]:
-                            gripper_state = observation["gripper_states"]
-                            ee_state = observation["ee_states"]
-                            state = (np.asarray(np.concatenate((ee_state, gripper_state), axis=-1), np.float32),)
-                            # TODO: draw path with opencv2 here, along with the subtrajectory versions.
-                            # TODO: save the significant/stopping points? or should we just construct the masks now?
-                            # TODO: what else to save?
-                            gripper_path = points_and_masks_h5[]
-                            dataset.add_frame(
-                                {
-                                    "image": observation["agentview_image"][
-                                        ::-1
-                                    ],  # flip the image as it comes from LIBERO reversed
-                                    "wrist_image": observation["eye_in_hand_rgb"][
-                                        ::-1
-                                    ],  # flip the image as it comes from LIBERO reversed
-                                    "state": state,
-                                    "actions": f["data"][demo_num]["action"],
-                                }
-                            )
-                        # compute language instruction
-                        if "problem_info" in f["data"].attrs:
-                            command = json.loads(f["data"].attrs["problem_info"])["language_instruction"]
-                        else:
-                            # openvla's language instruction extraction method as it doesn't have problem_info
-                            raw_file_string = os.path.basename(libero_h5_file).split("/")[-1]
-                            words = raw_file_string[:-10].split("_")
-                            command = ""
-                            for w in words:
-                                if "SCENE" in w:
-                                    command = ""
-                                    continue
-                                command = command + w + " "
-                            command = command[:-1]
+    for raw_dataset_name in RAW_DATASET_NAMES:
+        # open the directory containing the h5 files using raw_dataset_name
+        libero_h5_list = [file for file in os.listdir(Path(data_dir) / raw_dataset_name) if file.endswith(".hdf5")]
+        for libero_h5_file in libero_h5_list:
+            with h5py.File(Path(data_dir) / raw_dataset_name / libero_h5_file, "r", swmr=True) as f:
+                for demo_name in enumerate(f["data"].keys()):
+                    masks, paths, subtask_paths, quests = get_mask_and_path_from_h5(
+                        annotation_path=Path(path_and_mask_file_dir) / "dataset_movement_and_masks.h5",
+                        task_key=libero_h5_file.split(".")[0],
+                        observation=f["data"][demo_name]["obs"],
+                        demo_key=demo_name,
+                        hi_start=0,
+                        hi_end=len(f["data"][demo_name]["obs"]),
+                    )
+
+                    # Compute the main language instruction
+                    if "problem_info" in f["data"].attrs:
+                        command = json.loads(f["data"].attrs["problem_info"])["language_instruction"]
+                    else:
+                        # openvla's language instruction extraction method as it doesn't have problem_info
+                        raw_file_string = os.path.basename(libero_h5_file).split("/")[-1]
+                        words = raw_file_string[:-10].split("_")
+                        command = ""
+                        for w in words:
+                            if "SCENE" in w:
+                                command = ""
+                                continue
+                            command = command + w + " "
+                        command = command[:-1]
+
+                    # Track subtask instructions to divide episodes
+                    current_subtask = None
+
+                    obs_len = len(f["data"][demo_name]["obs"]["ee_pos"])
+
+                    assert (
+                        len(masks)
+                        == len(paths)
+                        == len(subtask_paths)
+                        == len(quests)
+                        == obs_len
+                        == len(f["data"][demo_name]["action"])
+                    ), "Lengths of mask, path, subtask_path, quests, ee_pos, and action must match"
+
+                    for i, observation in enumerate(f["data"][demo_name]["obs"]):
+                        gripper_state = observation["gripper_states"]
+                        ee_state = observation["ee_states"]
+                        state = (np.asarray(np.concatenate((ee_state, gripper_state), axis=-1), np.float32),)
+
+                        dataset.add_frame(
+                            {
+                                "image": observation["agentview_image"][
+                                    ::-1
+                                ],  # flip the image as it comes from LIBERO reversed
+                                "wrist_image": observation["eye_in_hand_rgb"][
+                                    ::-1
+                                ],  # flip the image as it comes from LIBERO reversed
+                                "mask": masks[i],
+                                "path": subtask_paths[i] if use_subtask_path else paths[i],
+                                "state": state,
+                                "actions": f["data"][demo_name]["action"],
+                            }
+                        )
+
+                        # Determine current subtask instruction (if using subtask instructions)
+                        if use_subtask_instructions and quests:
+                            # Get the subtask for this frame directly
+                            new_subtask = quests[i]
+
+                            # If subtask changed or this is the last frame, save the episode
+                            if (current_subtask is not None and new_subtask != current_subtask) or i == obs_len - 1:
+                                # Save episode with current subtask instruction
+                                dataset.save_episode(task=current_subtask)
+                                current_subtask = new_subtask
+                            # Initialize current_subtask if this is the first frame
+                            elif current_subtask is None:
+                                current_subtask = new_subtask
+
+                    # If not using subtask instructions, save the entire episode at once
+                    if not use_subtask_instructions:
                         dataset.save_episode(task=command)
 
     # Consolidate the dataset, skip computing stats since we will do that later
