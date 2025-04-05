9551d39f8eb492dd71dfc515c5612bad80896434
Jesse Zhang <jessezhang5@gmail.com>
2025-04-04 20:40:46 -0700

    fix

--- src/openpi/policies/mask_path_utils.py
+++ src/openpi/policies/mask_path_utils.py
@@ -260,8 +260,7 @@ def get_mask_and_path_from_h5(
         stopped_points = f_annotation["stopped_points"][i]
         # movement_key = "movement_across_video" # "movement_across_subtrajectory"
         # movement_across_video = f_annotation[movement_key]
-        mask_points = np.concatenate([significant_points, stopped_points], axis=1)
-        breakpoint()
+        mask_points = np.concatenate([significant_points, stopped_points], axis=0)
         # mask the image with the mask
         empty_img = np.zeros_like(images[i])
         mask = add_mask_2d_to_img(empty_img, mask_points)
