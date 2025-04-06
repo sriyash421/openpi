0112eb79da716e2ff84efc31108f88614df043a7
Jesse Zhang <jessezhang5@gmail.com>
2025-04-05 21:53:52 -0700

    3 fsdp

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -611,7 +611,7 @@ _CONFIGS = [
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
-        fsdp_devices=2,
+        fsdp_devices=3,
         batch_size=96,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
@@ -637,7 +637,7 @@ _CONFIGS = [
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
-        fsdp_devices=2,
+        fsdp_devices=3,
         batch_size=96,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
@@ -663,7 +663,7 @@ _CONFIGS = [
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
-        fsdp_devices=2,
+        fsdp_devices=3,
         batch_size=96,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
