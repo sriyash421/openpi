f2f26897cd86f834739cbacfc0b868406bad49ae
Jesse Zhang <jessezhang5@gmail.com>
2025-04-06 22:20:08 -0700

    fix for the batch size to make it 96 PER gpu

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -612,7 +612,7 @@ _CONFIGS = [
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
         fsdp_devices=3,
-        batch_size=96,
+        batch_size=288,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
         # for the given model config for LoRA finetuning. Just make sure it matches the model config
@@ -638,7 +638,7 @@ _CONFIGS = [
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
         fsdp_devices=3,
-        batch_size=96,
+        batch_size=288,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
         # for the given model config for LoRA finetuning. Just make sure it matches the model config
@@ -664,7 +664,7 @@ _CONFIGS = [
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
         num_train_steps=100_000,
         fsdp_devices=3,
-        batch_size=96,
+        batch_size=288,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
         # for the given model config for LoRA finetuning. Just make sure it matches the model config
