ca813fc339bfb5928805b548d1d249fd845abad0
Jesse Zhang <jessezhang5@gmail.com>
2025-04-07 22:47:38 -0700

    update the path and masking

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -553,7 +553,7 @@ _CONFIGS = [
         # Here is an example of loading a pi0 model for LoRA fine-tuning.
         model=pi0.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotLiberoDataConfig(
-            repo_id="jesbu1/libero_90_lerobot_pathmask",
+            repo_id="jesbu1/libero_test_lerobot_pathmask_rdp",
             base_config=DataConfig(
                 local_files_only=True,  # Set to True for local-only datasets.
                 prompt_from_task=True,
