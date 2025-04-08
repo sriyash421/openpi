d86e2830d4e50fcba96efc78be27703212ef1812
Jesse Zhang <jessezhang5@gmail.com>
2025-04-07 23:22:43 -0700

    update configs to use rdp

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -589,7 +589,7 @@ _CONFIGS = [
         # Here is an example of loading a pi0 model for LoRA fine-tuning.
         model=pi0.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotLiberoDataConfig(
-            repo_id="jesbu1/libero_90_lerobot_pathmask",
+            repo_id="jesbu1/libero_90_lerobot_pathmask_rdp",
             base_config=DataConfig(
                 local_files_only=True,  # Set to True for local-only datasets.
                 prompt_from_task=True,
@@ -622,7 +622,7 @@ _CONFIGS = [
         # Here is an example of loading a pi0 model for LoRA fine-tuning.
         model=pi0.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotLiberoDataConfig(
-            repo_id="jesbu1/libero_90_lerobot_pathmask",
+            repo_id="jesbu1/libero_90_lerobot_pathmask_rdp",
             base_config=DataConfig(
                 local_files_only=True,  # Set to True for local-only datasets.
                 prompt_from_task=True,
@@ -655,7 +655,7 @@ _CONFIGS = [
         # Here is an example of loading a pi0 model for LoRA fine-tuning.
         model=pi0.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotLiberoDataConfig(
-            repo_id="jesbu1/libero_90_lerobot_pathmask",
+            repo_id="jesbu1/libero_90_lerobot_pathmask_rdp",
             base_config=DataConfig(
                 local_files_only=True,  # Set to True for local-only datasets.
                 prompt_from_task=True,
