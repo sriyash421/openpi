7305eb43a3056ac239532932a58a9166a48aa4b6
Jesse Zhang <jessezhang5@gmail.com>
2025-04-15 15:38:05 -0700

    fix old import

--- examples/libero/main.py
+++ examples/libero/main.py
@@ -18,7 +18,7 @@ import tyro
 import sys
 
 sys.path.append(".")
-from eval_maskpath_utils import get_path_mask_from_vlm
+from .eval_maskpath_utils import get_path_mask_from_vlm
 
 LIBERO_DUMMY_ACTION = [0.0] * 6 + [-1.0]
 LIBERO_ENV_RESOLUTION = 256  # resolution used to render training data
