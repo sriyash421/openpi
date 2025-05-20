9ca5207999484df0354ab48030206777e52430b7
Jesse Zhang <jessezhang5@gmail.com>
2025-05-19 17:40:53 -0700

    48 h

--- run_scripts/train_usc_widowx_expert.sh
+++ run_scripts/train_usc_widowx_expert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=jessetho_1023
 #SBATCH --nodes=1
-#SBATCH --time=35:00:00
+#SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=185G
 #SBATCH --partition=gpu
--- run_scripts/train_usc_widowx_expert_2.sh
+++ run_scripts/train_usc_widowx_expert_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=jessetho_1023
 #SBATCH --nodes=1
-#SBATCH --time=14:00:00
+#SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=185G
 #SBATCH --partition=gpu
